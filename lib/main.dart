31cfa286510fe01871c98de040a9571c469cf218
Somtochukwu Nwagbata <healer2030@gmail.com>
2021-10-23 03:39:13 -0400

    main class - needs to properly implement auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:zara/services/auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MultiProvider(providers: [], child: const MyApp()));
+  runApp(
+    MultiProvider(
+      providers: [
+        // Provider is used for simple state management throughout the app
+        // Will most likely need it for auth and retrieving data from firestore
+        ChangeNotifierProvider<Auth>(
+          create: (context) => Auth(),
+        ),
+        StreamProvider(
+          create: (context) => Auth().user,
+          initialData: null,
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-    );
+    return Consumer<Auth>(builder: (context, auth, child) {
+      return MaterialApp(
+        title: 'Zara',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+      );
+    });
   }
 }
