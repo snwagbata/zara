f9a1d53e7eca28d62485a61072d670137b7dab5a
Somtochukwu Nwagbata <healer2030@gmail.com>
2021-10-23 06:39:29 -0400

    Logo widget implemeted; just need to design logo

---
+++ lib/components/logo.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String _imageLogo = 'assets/images/logo.png';
+    if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
+      _imageLogo = 'assets/images/logo.png';
+    }
+    return Hero(
+      tag: 'App logo',
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 36.0,
+          child: Image.asset(_imageLogo),
+        ),
+      ),
+    );
+  }
+}
