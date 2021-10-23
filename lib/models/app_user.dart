c6ff9f99be6e87fcf521e951361ac9d85a08eddc
Somtochukwu Nwagbata <healer2030@gmail.com>
2021-10-23 18:20:21 -0400

    renaming

--- lib/models/app_user.dart
+++ lib/models/app_user.dart
@@ -2,20 +2,20 @@ class AppUser {
   final String uid;
   final String name;
   final String email;
-  final List<String> classes;
+  final List<String> courses;
 
   AppUser(
       {required this.uid,
       required this.name,
       required this.email,
-      required this.classes});
+      required this.courses});
 
   factory AppUser.fromMap(Map<String, dynamic> data) {
     return AppUser(
       uid: data['uid'],
       name: data['name'],
       email: data['email'],
-      classes: List.from(data['classes']),
+      courses: List.from(data['courses'] ?? []),
     );
   }
 
@@ -24,7 +24,7 @@ class AppUser {
       'uid': uid,
       'name': name,
       'email': email,
-      'classes': classes,
+      'courses': courses,
     };
   }
 }
